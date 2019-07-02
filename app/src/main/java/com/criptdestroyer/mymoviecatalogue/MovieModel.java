a323104e194f2f58b984b675a27ddbf22b5e3964
Criptdestroyer <ahmademir1201@gmail.com>
2019-07-03 04:34:41 +0700

    init project

---
+++ app/src/main/java/com/criptdestroyer/mymoviecatalogue/MovieModel.java
@@ -0,0 +1,11 @@
+package com.criptdestroyer.mymoviecatalogue;
+
+public class MovieModel {
+    private String title;
+    private String description;
+    private String image;
+
+    public MovieModel(String title, String description, String image){
+        this.
+    }
+}
