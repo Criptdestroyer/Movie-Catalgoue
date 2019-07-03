b22a3e1591b5f3cd0f824fd215c1d278a4fd009f
Criptdestroyer <ahmademir1201@gmail.com>
2019-07-03 07:53:55 +0700

    layout detail movie done

--- app/src/main/java/com/criptdestroyer/mymoviecatalogue/DetailMovieActivity.java
+++ app/src/main/java/com/criptdestroyer/mymoviecatalogue/DetailMovieActivity.java
@@ -2,9 +2,10 @@ package com.criptdestroyer.mymoviecatalogue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailMovieActivity extends AppCompatActivity {
-
+    TextView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
